9400cf4adaf68ca797c7e22062b71fd27f14a894
Profe Damian <damian@danielcastelao.org>
2023-01-16 10:51:38 +0100

    Capturar foto

--- app/src/main/java/com/dam2/ejemplosintent/MainActivity.kt
+++ app/src/main/java/com/dam2/ejemplosintent/MainActivity.kt
@@ -2,31 +2,40 @@ package com.dam2.ejemplosintent
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 
 
 class MainActivity : Activity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // El botón para iniciar el teléfono
+        // El botón para iniciar la camara
         val callButton = findViewById<Button>(R.id.call)
-        // numero de telefono al que queremos llamar
-        val url = findViewById<EditText>(R.id.url)
 
 
         callButton.setOnClickListener{
             // creamos un intent específico que iniciará el teléfono
-            val intent = Intent(Intent.ACTION_VIEW)
-            // rellenamos el intent con el número
-            intent.data = Uri.parse(url.text.toString())
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             // llamamos a la activity treléfono
-            startActivity(intent)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && data != null) {
+            val imageBitmap = data.extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
         }
     }
 }
