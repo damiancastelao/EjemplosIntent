5f70d7c1b5dae8e4c71661ecf664e638e85dffb1
Profe Damian <damian@danielcastelao.org>
2023-01-16 08:48:28 +0100

    fix code

--- app/src/main/java/com/dam2/ejemplosintent/MainActivity.kt
+++ app/src/main/java/com/dam2/ejemplosintent/MainActivity.kt
@@ -6,11 +6,9 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
